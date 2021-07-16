db2cee54321b37d95056d97382cf023e47e7731a
foxandrewd <fox.andrew.d@gmail.com>
2021-07-16 13:47:02 +1000

    Updated

--- netCDF_to_csv.py
+++ netCDF_to_csv.py
@@ -5,17 +5,19 @@ Created on Thu May 27 21:45:49 2021
 """
 
 from netCDF4 import Dataset
-from netCDF4 import num2date
 import sys, os
 import pandas as pd
+import numpy as np
+import random as rnd
 import warnings
+import yaml
 from collections import OrderedDict, defaultdict
 warnings.filterwarnings("ignore")
+import datetime as dt
 
-data_folder = "C:/Users/Barbf/OneDrive/Documents/andy/ANZ/proj8/data/NODC"
-data_folder = data_folder.replace( "\\", "/" ) # change any backslashes to forwardslashes
+data_folder = "./data"
 exemplar = "NDBC_41001_202101_D4_v00.nc" # File to use as the 'standard' format file
-MAX_FILES_TO_RUN = 5
+MAX_FILES_TO_RUN = 1
 
 def main():
     _,_,files = next(os.walk(data_folder))
@@ -25,9 +27,10 @@ def main():
     exemplar_fields = analyse_fields(exemplar_fields_raw, exemplar_fields_raw, True)[0][exemplar]
     
     for ds in files[:MAX_FILES_TO_RUN]:
-        curr_fields = convert_netCDF_file_to_csv(data_folder, ds)
-        fields[ds] = curr_fields[:]
-        
+        if ds.endswith(".nc"):
+            curr_fields = convert_netCDF_file_to_csv(data_folder, ds)
+            fields[ds] = curr_fields[:]
+            
     new_fields,new_counts = analyse_fields(fields, exemplar_fields, False)
     print()
     analyse_fields_order(new_fields, exemplar_fields)
@@ -80,28 +83,32 @@ def analyse_fields(fields, exemplar_fields, is_exemplar):
     exemplar_fields_set = set(exemplar_fields)
     
     if not is_exemplar:
-        print('\t'.join(['Dataset', '#Fields', '#Standard', '%Standard']))
+        print('\n'+'\t'.join(['Dataset', '#Fields', '#Standard', '#MatchStandard', '%Matched']))
     for ds in new_fields:  # ds stands for dataset(name)
         ds_fields_set = set(new_fields[ds])
         num_desired   = len(exemplar_fields_set.intersection(ds_fields_set))
         perc_desired  = round(100*num_desired/len(exemplar_fields_set), 1)
         if not is_exemplar:
-            print( '\t'.join([ds, str(len(new_fields[ds])) , str(num_desired), str(perc_desired)+"%"]))
+            print( '\t'.join([ds, str(len(new_fields[ds])), str(len(exemplar_fields_set)), str(num_desired), str(perc_desired)+"%"]))
     return new_fields, new_counts
 
+
 def convert_netCDF_file_to_csv(data_folder, ds):
+    outfileName = ds.replace(".nc",".csv")
     x = Dataset(data_folder + "/" + ds, "r", format="NETCDF4")
     times = x['time'][:]
     colnames = ['base0.base1.datetime']
-    fields_1 = OrderedDict();  fields_1['datetime.1'] = 1;
-    fields_2 = OrderedDict();  fields_2['datetime.1'] = 1;
+    fields_1 = OrderedDict();  fields_1['datetime.1'] = True
+    fields_2 = OrderedDict();  fields_2['datetime.1'] = True
     
     for var in x.variables:
         vardata = x[var][:]
-        if len(vardata) == len(times): # Add in if variable has the same length as 'times'
-            colnames.append("base0.base1."+var)
-            fields_1[var+".1"] = 1
-            
+        colnames.append("base0.base1."+var)
+        curr_var_num_try = 1
+        while var+"."+str(curr_var_num_try) in fields_1:
+            curr_var_num_try += 1
+        fields_1[var+"."+str(curr_var_num_try)] = True
+
     for pload in x.groups.keys():
         for sensor in x[pload].groups.keys():
             sensor_data = x.createGroup(pload).createGroup(sensor)
@@ -110,38 +117,57 @@ def convert_netCDF_file_to_csv(data_folder, ds):
                 curr_attrib_num_try = 1
                 while attrib+"."+str(curr_attrib_num_try) in fields_1:
                     curr_attrib_num_try += 1
-                fields_1[attrib+"."+str(curr_attrib_num_try)] = 1
-    
-    # Now have the column/field names, so can now make the DataFrame:
+                fields_1[attrib+"."+str(curr_attrib_num_try)] = True
+
+    # Abort if NetCDF file has already been converted to csv:
+    #if os.path.isfile(data_folder+"/csv/"+outfileName): return colnames
+
+    # We now have the column/field names, so can now make the DataFrame:
     df = pd.DataFrame(columns = fields_1.keys(), index=times)
     df = df.fillna(0)
-    df['datetime.1'] = num2date(times, x['time'].units)
+    df['datetime.1'] = [dt.datetime.utcfromtimestamp(x).strftime('%Y-%m-%d %H:%M:%S') for x in times]
     
     for var in x.variables:
         vardata = x[var][:]
-        if len(vardata) == len(times):
-            df[var + ".1"] = vardata
-    
+        if type(vardata) == np.ma.core.MaskedArray:
+            vardata = vardata.data.tolist()[0]
+        try:
+            curr_var_num_try = 1
+            while var+"."+str(curr_var_num_try) in fields_2:
+                curr_var_num_try += 1
+            fields_2[var+"."+str(curr_var_num_try)] = True
+            df[var+"."+str(curr_var_num_try)] = vardata
+        except:
+            print("Err_1: ", var, len(vardata))
+            #print(vardata)
+            #print(type(vardata))
+            pass
+
+    df['time.1'] = times
+    try: df['time_wpm_20.1'] = times
+    except: pass
+
     for pload in x.groups.keys():
         for sensor in x[pload].groups.keys():
             sensor_data = x.createGroup(pload).createGroup(sensor)
             for attrib in sensor_data.variables.keys():
-                #try: df[pload + "." + sensor + "." + attrib] = sensor_data[attrib][:]
-                #except: pass
                 try:
+                    attrib_data = sensor_data[attrib][:]
                     curr_attrib_num_try = 1
                     while attrib+"."+str(curr_attrib_num_try) in fields_2:
                         curr_attrib_num_try += 1
-                    fields_2[attrib+"."+str(curr_attrib_num_try)] = 1
-                    df[attrib+"."+str(curr_attrib_num_try)] = sensor_data[attrib][:]
-                except: pass
+                    fields_2[attrib+"."+str(curr_attrib_num_try)] = True
+                    df[attrib+"."+str(curr_attrib_num_try)] = attrib_data
+                except:
+                    print("Err_2: ", attrib, curr_attrib_num_try)
+                    print(attrib_data)
     
     # Save data to CSV file format in subdirectory 'csv/' in the data folder:
     outfileName = ds.replace(".nc",".csv")
     if not os.path.isdir(data_folder+"/csv"): os.mkdir(data_folder+"/csv")
     df.to_csv(data_folder + "/csv/" + outfileName)
-    
     return colnames
 
 if __name__ == "__main__":
-   main()
+    # run main function:
+    main()
