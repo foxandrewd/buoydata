b2d95fe508ff36262a20c5811206646948fc169a
foxandrewd <fox.andrew.d@gmail.com>
2021-08-12 19:02:12 +1000

    Added params dict interface with gui.py

--- buoy_generator.py
+++ buoy_generator.py
@@ -1,5 +1,5 @@
 """
-Created on: Thursday May 27, 2021
+Created on: August 12, 2021
 @author: Andrew Fox
 """
 
@@ -9,7 +9,8 @@ import numpy as np
 import random as rnd
 import datetime as dt
 import re
-
+import warnings
+warnings.filterwarnings("ignore")
 
 base_folder = 'C:/Users/Barbf/Downloads/cmanwx'
 
@@ -18,8 +19,12 @@ BASIS_MONTH = '01'
 
 MAX_FILES_TO_RUN = 1
 
+SIM_PREFIX = 'sim'
+
 OUTPUT_YEARS = [2022]
 OUTPUT_MONTHS = [1,2,3,4,5,6,7,8,9,10,11,12]
+
+DATA_FREQ_IN_HOURS = 1
 
 # Zero'th month doesn't exist:
 C_MonthLength = [0,31,28,31,30,31,30,31,31,30,31,30,31]
@@ -35,21 +40,42 @@ def extract_buoy_names(fnames):
         else: continue
     return buoy_fnames
 
-def main():
+def main(params):
     
-    #basis_csv_folder = '/'.join([base_folder,BASIS_YEAR,BASIS_MONTH,'csv'])
-    #(_,_,files) = next(os.walk(basis_csv_folder))
-    #buoynames = extract_buoy_names(files)
-    #print(buoynames)
-    #return
+    global base_folder, BASIS_YEAR, BASIS_MONTH, MAX_FILES_TO_RUN
+    global SIM_PREFIX, OUTPUT_YEARS, OUTPUT_MONTHS, DATA_FREQ_IN_HOURS
+
+    OUTPUT_YEARS = params['outyears']
+    OUTPUT_MONTHS = params['outmonths']
+    BASIS_YEAR = params['basisyear']
+    BASIS_MONTH = params['basismonth']
+    base_folder = params['basisfolder']
+    MAX_FILES_TO_RUN = params['numbuoys']
+    SIM_PREFIX = params['simprefix']
+    DATA_FREQ_IN_HOURS = params['datafreqinhours']
+
+
+    print()
+    print('0 base_folder: ', base_folder)
+    print('0 MAX_FILES_TO_RUN: ', MAX_FILES_TO_RUN)
+    print('0 OUTPUT_YEARS: ', OUTPUT_YEARS)
+    print('0 OUTPUT_MONTHS: ', OUTPUT_MONTHS)
+    print('0 SIM_PREFIX: ', SIM_PREFIX)
+    print('0 DATA_FREQ_IN_HOURS: ', DATA_FREQ_IN_HOURS)
+
     
-    #'''
+    '''basis_csv_folder = '/'.join([base_folder,BASIS_YEAR,BASIS_MONTH,'csv'])
+    (_,_,files) = next(os.walk(basis_csv_folder))
+    buoynames = extract_buoy_names(files)
+    print(buoynames)
+    return'''
+    
     for month in OUTPUT_MONTHS:
         mon = str(month).zfill(2)
         (_,_,files) = next(os.walk('/'.join([base_folder, BASIS_YEAR, mon, 'csv'])))
         for fname in files[:MAX_FILES_TO_RUN]:
             run_analysis(BASIS_YEAR, fname, BASIS_YEAR, month)
-    #'''
+    
     
     # Generate simulated data for all output years and months:
     for year in OUTPUT_YEARS:
@@ -58,8 +84,8 @@ def main():
             (_,_,files) = next(os.walk('/'.join([base_folder, BASIS_YEAR, mon, 'csv'])))                    
             for fname in files[:MAX_FILES_TO_RUN]:
                 datagen(BASIS_YEAR, fname, year, month)
-
-
+                
+    
 def run_analysis(basis_yr, fname, year, month):
     yr = str(year)
     mon = str(month).zfill(2)
@@ -143,7 +169,7 @@ def is_integer_column(col):
 
 def sampleN_data_dist(N, dist):
     dist = yaml.load(dist)
-    print('distrib: ', dist)
+    #print('distrib: ', dist)
     return [sample_from_dist(dist) for i in range(N)]
 
 def sample_from_dist(dist):
@@ -156,34 +182,41 @@ def sample_from_dist(dist):
     return dist.keys()[-1]
 
 def fname_to_buoyname(fname):
-    if fname.startswith('sim_'): # Remove 'sim_' if at the start of fname
-        fname = fname[4:]
-    if fname.startswith('NDBC_'):
-        newname = fname[5:]      # Remove 'NDBC_' from the start of the name
-    else:
-        newname = fname
+    
+    newname = fname
+    
+    if newname.startswith('simdata_'): # Remove 'simdata_' if at the start of fname
+        newname = newname[8:]
+    if newname.startswith('sim_'):
+        newname = newname[4:]      # Remove 'sim_' from the start of the name
+
+    if newname.startswith('NDBC_'):
+        newname = newname[5:]      # Remove 'NDBC_' from the start of the name
+    
     uscore = newname.index('_')
     buoyname = newname[:uscore]
     return buoyname
 
 def datagen(basis_yr, fname, year, month):
+
     yr = str(year)
     mon = str(month).zfill(2)
     yrmon = yr+mon # e.g. '202501'
     analysis_folder = '/'.join([base_folder,basis_yr,mon,'analysis'])
     
     df = pd.read_csv(analysis_folder + "/analysis_" + fname, index_col=0)
-    cols = list(df['FieldName'])    
+    cols = list(df['FieldName'])
     outdata = pd.DataFrame(columns=cols)
     
     start_ts = int(df['Min']['time.1'])
     start_dt = dt.datetime.utcfromtimestamp(start_ts)
     
     lastday = C_MonthLength[month]
-    N = 24 * lastday      # We sample every day, once for each hour
+    N = int(24 * lastday / DATA_FREQ_IN_HOURS)      # We sample every day, once for each hour, div by num hours diff to sample by
 
     start_dt = dt.datetime(year, month, 1, 0, 0, 0, tzinfo=dt.timezone.utc)
     start_ts = int(dt.datetime.timestamp(start_dt))
+    
     end_dt = dt.datetime(year, month, lastday, 23, 0, 0, tzinfo=dt.timezone.utc)
     end_ts = int(dt.datetime.timestamp(end_dt))
 
@@ -201,6 +234,7 @@ def datagen(basis_yr, fname, year, month):
             outdata[col] = pd.Series([rnd.normalvariate(mu,sigma) for i in range(N)])
 
     time_diff = 3600 # The number of seconds in 1 hour (60 x 60)
+    time_diff *= DATA_FREQ_IN_HOURS   # If freq is 2hrs then diff is 3600*2 = 7200 seconds
     times = [x for x in range(start_ts, end_ts+1, time_diff)] # Must add 1 to end_ts cos of how range() works
     datetimes = [dt.datetime.utcfromtimestamp(t).strftime('%Y-%m-%d %H:%M:%S') for t in times]
     
@@ -218,11 +252,19 @@ def datagen(basis_yr, fname, year, month):
     m = re.search("_[0-9]{6}", fname)  # find year-month e.g: '_200507' in the fname
     output_name = fname.replace(m[0], "_"+yrmon) # Replace old date with new (yrmon)
     
-    output_fname = '_'.join(["sim", buoyname, yrmon ]) + ".csv"
+    output_fname = '_'.join([SIM_PREFIX, buoyname, yrmon ]) + ".csv"
     output_folder = '/'.join([base_folder, yr, mon, 'csv'])
     os.makedirs(output_folder, exist_ok=True)
     outdata.to_csv( output_folder + '/' + output_fname)
 
+    print()
+    print('base_folder: ', base_folder)
+    print('MAX_FILES_TO_RUN: ', MAX_FILES_TO_RUN)
+    print('OUTPUT_YEARS: ', OUTPUT_YEARS)
+    print('OUTPUT_MONTHS: ', OUTPUT_MONTHS)
+    print('SIM_PREFIX: ', SIM_PREFIX)
+    print('DATA_FREQ_IN_HOURS: ', DATA_FREQ_IN_HOURS)
+
 
 if __name__ == "__main__":
-    main()
+    main(None)
